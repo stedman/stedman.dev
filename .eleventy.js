5dfabea3a1d6c58a6f2b1dcbc9ea7c5216592667
Steve Stedman <steve@stedman.dev>
2020-05-07 12:13:16 -0500

    Remove external-link transform

--- .eleventy.js
+++ .eleventy.js
@@ -56,43 +56,6 @@ module.exports = function (eleventyConfig) {
   // FILTERS
   eleventyConfig.addFilter('fullDate', filters.fullDate);
 
-  // TRANSFORM: Add appropriate TARGET and REL to external links.
-  eleventyConfig.addTransform('external-link-rel', (content) => {
-    const desired = {
-      className: 'external-link',
-      target: 'target="_blank"',
-      rel: 'rel="nofollow noopener noreferrer"',
-    };
-    // Find all external links--lazily we'll assume those start with https.
-    const reLinkMatch = /<a .*?href="https?:\/\/[^"]+".*?>/g;
-    // Find class, target, and rel attributes.
-    const reClass = /.*class="([^"]+)".*/;
-    const reTarget = /.*target="([^"]+)".*/;
-    const reRel = /.*rel="([^"]+)".*/;
-
-    return content.replace(reLinkMatch, (linkMatch) => {
-      const classMatch = linkMatch.match(reClass);
-      let newLink = linkMatch;
-
-      if (classMatch) {
-        newLink = linkMatch.replace(classMatch[1], `${classMatch[1]} ${desired.className}`);
-      } else {
-        newLink = linkMatch.replace('<a ', `<a class="${desired.className}" `);
-      }
-
-      const hasTarget = reTarget.test(newLink);
-
-      if (hasTarget && reRel.test(newLink)) {
-        // If target and rel already exist, assume it was done right.
-        return newLink;
-      } if (hasTarget) {
-        return newLink.replace('>', ` ${desired.rel}>`);
-      }
-
-      return newLink.replace('>', ` ${desired.target} ${desired.rel}>`);
-    });
-  });
-
   return {
     dir: {
       input: 'src',
