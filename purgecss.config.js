676e750cf3b84876669b0a4799733c241680e45d
Steve Stedman <steve@stedman.dev>
2021-09-06 16:32:04 -0500

    Update PurgeCSS 2.3.0 -> 4.0.3

---
+++ purgecss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  css: ['dist/assets/css/main.css'],
+  content: [
+    'dist/index.html',
+    'dist/about/index.html',
+    'dist/20*/**/*.html',
+  ],
+  output: 'dist/assets/css/main.css',
+};
